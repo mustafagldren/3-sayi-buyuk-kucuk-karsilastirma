acfc5c4c75d02b808b8e6c12810fbefdd71f83e5
c0admin <mustafagldren@gmail.com>
2023-01-11 01:17:39 +0300

    Proje dosyası ekle.

---
+++ project/project.cpp
@@ -0,0 +1,56 @@
+﻿/*Kullanıcıdan 3 sayı girmesini istiyoruz.Sayılardan küçük olanlardan 2 tanesini topluyoruz.
+Ve sonucu en büyük sayı ile karşılaştırıyoruz. Eğer 2 sayının toplamı ,en büyük sayıdan büyükse
+ekrana "tebrikler", değilse "maalesef" yazdırıyoruz.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, x;
+    cout << "Birinci sayiyi giriniz :";
+    cin >> a;
+    cout << "İkinci sayiyi giriniz :";
+    cin >> b;
+    cout << "Ucuncu sayiyi giriniz :";
+    cin >> c;
+
+    if (a > b && a > c) {
+        x = b + c;
+        if (x > a) {
+            cout << "Tebrikler" << endl;
+        }
+        else {
+            cout << "Maalesef." << endl;
+        }
+
+
+    }
+
+    if (b > a && b > c) {
+        x = a + c;
+        if (x > b) {
+            cout << "Tebrikler." << endl;
+        }
+        else {
+            cout << "Maalesef." << endl;
+        }
+    }
+
+    if (c > a && c > b) {
+        x = a + b;
+        if (x > c) {
+            cout << "Tebrikler." << endl;
+
+        }
+        else {
+            cout << "Maalesef." << endl;
+        }
+    }
+
+
+    system("pause");
+    return 0;
+
+}
+
